9410f33e7a2867fa16c197101e9f87ebfd10c35a
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-04-08 14:40:43 +0530

    change build fp

--- dot_guacamole.mk
+++ dot_guacamole.mk
@@ -14,13 +14,9 @@ $(call inherit-product, device/oneplus/guacamole/device.mk)
 # Inherit some common DotOS stuff.
 $(call inherit-product, vendor/dot/config/common.mk)
 
-TARGET_BOOT_ANIMATION_RES := 1440
 WITH_GAPPS := true
 TARGET_SUPPORTS_BLUR := true
 EXTRA_FOD_ANIMATIONS := true
-
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1440
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := dot_guacamole
@@ -38,10 +34,10 @@ TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
 # Build info
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ2A.210405.005/7181113:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7Pro \
-    PRODUCT_NAME=OnePlus7Pro \
-    PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
+    PRODUCT_NAME=OnePlus7Pro
+    PRIVATE_BUILD_DESC="coral-user 11 RQ2A.210405.005 7181113 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
